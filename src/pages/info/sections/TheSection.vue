4c30aae65b50ae2bb9d029e5e38e422388c32909
FrancescoBonafede <f.bona99@gmail.com>
2024-03-15 17:52:44 +0100

    test? non funzionerà mai

--- src/pages/info/sections/TheSection.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="border-dotted border-2 p-4 py-3">
-        <h3 class="text-xl font-bold">Questa è una sezione</h3>
-        <p>Ogni pagina ha la sua cartella con le sue sezioni <b>./sections/NomeSection.vue</b></p>
-    </div>
-</template>
